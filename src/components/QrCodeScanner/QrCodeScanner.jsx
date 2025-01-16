09366bed0f6b04005e074d6e8407139236a3aac8
Moonlight1z <alexxcactucc@gmail.com>
2025-01-17 00:55:53 +0300

    Добавление основной камеры на телефон и дополнительная проверка на значения в localstorage

--- src/components/QrCodeScanner/QrCodeScanner.jsx
+++ src/components/QrCodeScanner/QrCodeScanner.jsx
@@ -6,69 +6,69 @@ import './QrCodeScanner.scss';
 import copy from './img/copy.png'
 
 function QRCodeScanner() {
-    const [scanned, setScanned] = useState('');
-    const [copied, setCopied] = useState(false);
-  
-    const scanHandler = (result) => {
-      if (!result) return;
-  
-      const prevData = JSON.parse(localStorage.getItem(SCAN_DATA) || '[]');
-      // if (prevData.includes(result.text)) return; // првоекра на значения, которые уже есть в БД
+  const [scanned, setScanned] = useState('');
+  const [copied, setCopied] = useState(false);
 
-      setScanned(result.text);
-  
-      localStorage.setItem(
-        SCAN_DATA, JSON.stringify([...prevData, result.text])
-      );
-    };
-  
-    const handleCopyClick = () => {
-      if (!scanned.trim()) {
-        alert('Пожалуйста, сканируйте QR-код перед копированием.');
-        return;
-      }
-  
-      navigator.clipboard.writeText(scanned).then(() => {
-        setCopied(true);
-        setTimeout(() => setCopied(false), 1000);
-      }).catch(err => {
-        console.error('Ошибка при копировании:', err);
-        alert('Не удалось скопировать текст. Попробуйте еще раз.');
-      });
-    };
-  
-    return (
-      <div className="container">
-        <div className='qr-scanner'>
-          <QrReader
-            constraints={{fasingMode: 'environment'}}
-            scanDelay={1000}
-            onResult={scanHandler}
-            containerStyle={{ width: '380px' }}
-            className='qr-scanner__item'
-          />
-          <div className='qr-scanner__copy-text'>
-            <p className='result'>Результат сканирования:</p>
-            {scanned && (
-              <>
-                <p className=''>{scanned}</p>
-                <img 
-                  src={copy} 
-                  alt="copy" 
-                  width={25} 
-                  height={25}
-                  onClick={handleCopyClick}
-                  style={{cursor: 'pointer'}}
-                />
-              </>
-            )}
-            {copied && (
-              <div className='copied-message'>Текст скопирован!</div>
-            )}
-          </div>
+  const scanHandler = (result) => {
+    if (!result) return;
+
+    const prevData = JSON.parse(localStorage.getItem(SCAN_DATA) || '[]');
+    if (prevData.includes(result.text)) return; // првоекра на значения, которые уже есть в БД
+
+    setScanned(result.text);
+
+    localStorage.setItem(
+      SCAN_DATA, JSON.stringify([...prevData, result.text])
+    );
+  };
+
+  const handleCopyClick = () => {
+    if (!scanned.trim()) {
+      alert('Пожалуйста, сканируйте QR-код перед копированием.');
+      return;
+    }
+
+    navigator.clipboard.writeText(scanned).then(() => {
+      setCopied(true);
+      setTimeout(() => setCopied(false), 1000);
+    }).catch(err => {
+      console.error('Ошибка при копировании:', err);
+      alert('Не удалось скопировать текст. Попробуйте еще раз.');
+    });
+  };
+
+  return (
+    <div className="container">
+      <div className='qr-scanner'>
+        <QrReader
+          constraints={{ facingMode: 'user' }}
+          scanDelay={1000}
+          onResult={scanHandler}
+          containerStyle={{ width: '380px' }}
+          className='qr-scanner__item'
+        />
+        <div className='qr-scanner__copy-text'>
+          <p className='result'>Результат сканирования:</p>
+          {scanned && (
+            <>
+              <p className=''>{scanned}</p>
+              <img
+                src={copy}
+                alt="copy"
+                width={25}
+                height={25}
+                onClick={handleCopyClick}
+                style={{ cursor: 'pointer' }}
+              />
+            </>
+          )}
+          {copied && (
+            <div className='copied-message'>Текст скопирован!</div>
+          )}
         </div>
       </div>
-    );
-  }
-  
-  export default QRCodeScanner;
+    </div>
+  );
+}
+
+export default QRCodeScanner;
